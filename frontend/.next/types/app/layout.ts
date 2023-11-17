f0af8f5cc9839b290cfe123df28d723358bdfe3e
Lexthus <Lexthus@gmail.com>
2023-11-17 13:44:16 -0300

    Updated binary files and modified app and build manifests

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\NEXTJS\prueba\autogestion\frontend\app\layout.tsx
+// File: D:\autogestion\frontend\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
